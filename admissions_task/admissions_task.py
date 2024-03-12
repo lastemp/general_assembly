2542b493a1ac7ef043936a0e4a89e3570565b84a
lastemp <emmanuelochieng797@gmail.com>
2024-03-12 15:45:47 +0300

    2nd commit

--- admissions_task/admissions_task.py
+++ admissions_task/admissions_task.py
@@ -8,16 +8,8 @@ from scipy.stats import skew
 
 from scipy.stats import probplot
 
-# Maps names to a set of corresponding person_ids
-# names = {}
 
-# Maps person_ids to a dictionary of: name, birth, movies (a set of movie_ids)
-# people = {}
-
-# Maps movie_ids to a dictionary of: title, year, stars (a set of person_ids)
-# movies = {}
-
-# Maps to pledged amounts
+# Maps to pledged amounts of the kickstarter dataset
 kickstarter_dataset = []
 
 
